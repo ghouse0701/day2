3adde2c452dd440c0e6cfb008f6d4f5fbf9f5e2b
ghouse0701 <148069491+ghouse0701@users.noreply.github.com>
2023-11-23 18:49:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
-resource "local_file" "my_pets" {
-filename = var.filename
-content = var.content
+resource "aws_iam_user" "Admin-user" {
+  name = "mgm"
+  tags = {
+    "description" = "Technical Team Lead"
+  }
 }
-resource "random_pet" "my_pets" {
-prefix = "Miss"
-separator = "."
-length = "1"
+resource "aws_iam_policy" "adminuser" {
+  name   = "AdminUsers"
+  policy = file("admin-policy.json")
+}
+resource "aws_iam_user_policy_attachment" "mgm-admin-access" {
+  user       = aws_iam_user.Admin-user.name
+  policy_arn = aws_iam_policy.adminuser.arn
 }
